03b84b337b41672fdca701d1c9ede1884f58295e
Runako <runakowilliams@gmail.com>
2018-05-07 14:00:50 +0000

    Sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
   resources :articles
+
+
 end
