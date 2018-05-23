e88cd5c0ccefa6be29d90e8b5a11614546a8ece2
Runako <runakowilliams@gmail.com>
2018-05-23 14:52:19 +0000

    Create user show action and gravater image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
